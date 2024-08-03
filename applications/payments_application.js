472960e5946d0f1bf5a1119653d088ecc9dea75e
Tanmay <tanmay@Tanmays-MacBook-Pro.local>
2024-08-03 19:22:55 +0530

    adding ReadMe and base files for initial setup along with LLD and HLD Design proposals;

---
+++ applications/payments_application.js
@@ -0,0 +1,1 @@
+// This file will contain all the entry point application functionality of payments.
---
+++ applications/user_application.js
@@ -0,0 +1,1 @@
+// This file will contain all the entry point functionality of user applications
